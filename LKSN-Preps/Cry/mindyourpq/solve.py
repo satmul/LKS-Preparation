df2fc3fc783ea9b2c5764afcd772ab43bd9369ef
Bryan <56126330+satmul@users.noreply.github.com>
2023-09-03 15:34:05 +0700

    crypto

---
+++ LKSN-Preps/Cry/mindyourpq/solve.py
@@ -0,0 +1,21 @@
+from Crypto.Util.number import getPrime, inverse, long_to_bytes
+import binascii
+
+c = 964354128913912393938480857590969826308054462950561875638492039363373779803642185
+n = 1584586296183412107468474423529992275940096154074798537916936609523894209759157543
+e = 65537
+
+# n = p*q -> find the factor of the prime
+
+p = 2434792384523484381583634042478415057961
+q = 650809615742055581459820253356987396346063
+
+phi = (q-1)*(p-1)
+d = inverse(e, phi)
+
+#m = (c^d)%n
+m = pow( c, d, n )
+print(long_to_bytes(m))
+
+#print(repr(binascii.unhexlify(hex(m))))
+
